c57147b9ef3a6bebcfd9ef739146f667d5ecc34a
amer266030 <amer266030@users.noreply.github.com>
2024-07-31 10:50:16 +0300

    initial commit

---
+++ bin/github_test.dart
@@ -0,0 +1,5 @@
+import 'package:github_test/github_test.dart' as github_test;
+
+void main(List<String> arguments) {
+  print('Hello world: ${github_test.calculate()}!');
+}
---
+++ test/github_test_test.dart
@@ -0,0 +1,8 @@
+import 'package:github_test/github_test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
