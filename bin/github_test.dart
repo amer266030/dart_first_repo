bde3347b424f903ac45ce0ae9e3553fa2402b3b6
amer266030 <amer266030@users.noreply.github.com>
2024-07-31 11:16:40 +0300

    new function added

--- bin/github_test.dart
+++ bin/github_test.dart
@@ -3,3 +3,5 @@ import 'package:github_test/github_test.dart' as github_test;
 void main(List<String> arguments) {
   print('Hello world: ${github_test.calculate()}!');
 }
+
+void int() => 'some output';
